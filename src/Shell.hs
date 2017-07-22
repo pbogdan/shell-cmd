60319690991ab86fff3b7fe0206faa3c42226bca
Piotr Bogdan <ppbogdan@gmail.com>
2017-07-22 17:13:47 +0100

    Initial import.

---
+++ src/Shell.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+module Shell
+  ( ShellCmdFailed
+  , shell
+  , shellOut
+  , module Shell.Command
+  ) where
+
+import           Protolude
+
+import           Control.Monad.Logger
+import           Extra (systemOutput)
+import           Shell.Command
+import qualified Turtle.Prelude as P
+
+data ShellCmdFailed =
+  ShellCmdFailed ExitCode
+  deriving (Eq, Show)
+
+shell :: (MonadIO m, MonadLogger m, MonadError ShellCmdFailed m) => Text -> m ()
+shell cmd = do
+  ret <- P.shell cmd mempty
+  case ret of
+    ExitSuccess -> return ()
+    code@(ExitFailure _) -> do
+      logWarnN $ "Command " <> cmd <> " has failed with exit code " <> show code
+      throwError . ShellCmdFailed $ code
+
+shellOut ::
+     (MonadIO m, MonadLogger m, MonadError ShellCmdFailed m) => Text -> m Text
+shellOut cmd = do
+  (ret, out) <- liftIO $ systemOutput . toS $ cmd
+  case ret of
+    ExitSuccess -> return . toS $ out
+    code@(ExitFailure _) -> do
+      logWarnN $
+        "Command " <> toS cmd <> " has failed with exit code " <> show code
+      throwError . ShellCmdFailed $ code
---
+++ src/Shell/Command.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+module Shell.Command
+  ( option
+  , switch
+  , arg
+  , raw
+  , command
+  ) where
+
+import           Protolude
+
+import qualified Data.Text as Text
+import           System.Posix.Escape
+
+-- @TODO: think about providing short / log args / switches
+
+type Cmd = State [Text] ()
+
+option :: MonadState [Text] m => Text -> Text -> m ()
+option name value =
+  modify (\s -> name <> " " <> (toS . escape . toS $ value) : s)
+
+switch :: MonadState [Text] m => Text -> m ()
+switch name = modify (\s -> name : s)
+
+arg :: MonadState [Text] m => Text -> m ()
+arg name = modify (\s -> (toS . escape . toS $ name) : s)
+
+raw :: MonadState [Text] m => Text -> m ()
+raw name = modify (\s -> name : s)
+
+command :: Text -> Cmd -> Text
+command name x =
+  let s = runIdentity $ execStateT x [name]
+  in Text.intercalate " " (reverse s)
