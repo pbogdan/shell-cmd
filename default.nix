b16f55c1605167ff6d14547acb8f2a1517c04519
Piotr Bogdan <ppbogdan@gmail.com>
2020-08-06 15:45:06 +0100

    nix: clean up and refactor
    - make it possible to override the input `pkgs`
    - just use the default compiler, always

--- default.nix
+++ default.nix
@@ -1,34 +1,21 @@
-{ sources ? (import ./nix/sources.nix)
-, compiler ? "ghc882"
+let
+  sources = import ./nix/sources.nix;
+in
+{ pkgs ? import sources.unstable { }
 }:
 let
-  overlay = self: super: {
-    haskell = super.haskell // {
-      packages = super.haskell.packages // {
-        "${compiler}" = super.haskell.packages.${compiler}.override {
-          overrides = hself: hsuper: with self.haskell.lib; {};
-        };
-      };
-    };
+  hspkgs = pkgs.haskellPackages.override {
+    overrides = hself: hsuper: with pkgs.haskell.lib; { };
   };
 
-  pkgs = (
-    import sources.unstable {
-      overlays = [
-        overlay
-      ];
-    }
-  );
-
-  inherit (pkgs.haskellPackages)
+  inherit (hspkgs)
     callCabal2nix
-    shellFor
     ;
+
   inherit (pkgs)
-    lib
     nix-gitignore
     ;
 
-  shell-cmd = callCabal2nix "shell-cmd" (nix-gitignore.gitignoreSource [] ./.) {};
+  shell-cmd = callCabal2nix "shell-cmd" (nix-gitignore.gitignoreSource [ ] ./.) { };
 in
-  shell-cmd
+shell-cmd
